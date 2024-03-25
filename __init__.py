a7a2795e151a7c0b09f874b4428dba5696f36295
Reiniel Fernandez <reinielfc@gmail.com>
2024-03-25 19:17:36 -0400

    remove aliases

--- __init__.py
+++ __init__.py
@@ -1,7 +1,3 @@
-from .action import ActionBuilder as ACT, JunkScore, Priority
+from .action import ActionBuilder as ACT, JunkScore, Priority, Tag
 from .filter import Filter, FilterType, FilterList
 from .comparison import ComparisonListBuilder as PROP
-
-SENDER = PROP.sender
-SUBJECT = PROP.subject
-PROPX = PROP.expressionsearch
